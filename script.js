ef5a7f93c62be37174094c8ceab7d7271f68f6f1
amakom <obiajuluamakom@gmail.com>
2021-09-08 16:33:15 +0100

    Second Commit

--- script.js
+++ script.js
@@ -29,4 +29,15 @@ responsive: [
 ]
 });
 
+jQuery(document).ready(function( $ )
+{ $('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
+});
 
+window.addEventListener('scroll', function () {
+  let header = document.querySelector('header');
+  let windowPosition = window.scrollY > 700;
+  header.classList.toggle('scrolling-active', windowPosition);
+})
